ee40cdf8ab088b9f43450b277438f2a2e6e00845
Trishanth Diwate <trishanth@etherlabs.io>
2018-06-04 11:53:50 +0530

    added test account to bot

--- samples/sample_message.js
+++ samples/sample_message.js
@@ -20,7 +20,7 @@ nodeSDK.start().then( () => {
 }).then( () => {
     chatbot.onMessage((tag, step, content, from, done) => {
         console.log("::: On answer>", tag, step, content, from);
-    
+        console.log(nodeSDK.connectedUser);
         if(tag === "routing" && step === "choice" && content === "yes") {
             done("end_no");   
         } else {
@@ -33,6 +33,45 @@ nodeSDK.start().then( () => {
     }, this);
 });
 
+nodeSDK.events.on('rainbow_onready', function() {
+    // do something when the connection to Rainbow is up
+    let bubbles = nodeSDK.bubbles.getAll();
+    console.log(bubbles)
+});
 
+nodeSDK.bubbles._eventEmitter.on('rainbow_invitationreceived', (info) => {
+    console.log('#############')
+    console.log('Bubble invitation received', info)
+    console.log('#############')
+})
 
+nodeSDK.bubbles._eventEmitter.on('rainbow_affiliationchanged', (info) => {
+    console.log('#############')
+    console.log('Bubble rainbow_affiliationchanged received', info)
+    console.log('#############')
+})
+
+nodeSDK.bubbles._eventEmitter.on('rainbow_ownaffiliationchanged', (info) => {
+    console.log('#############')
+    console.log('Bubble rainbow ownaffiliation changed received', info)
+    console.log('#############')
+})
+
+nodeSDK.bubbles._eventEmitter.on('rainbow_customdatachanged', (info) => {
+    console.log('#############')
+    console.log('Bubble rainbow_customdatachanged received', info)
+    console.log('#############')
+})
+
+nodeSDK.bubbles._eventEmitter.on('rainbow_topicchanged', (info) => {
+    console.log('#############')
+    console.log('Bubble rainbow_topicchanged received', info)
+    console.log('#############')
+})
+
+nodeSDK.bubbles._eventEmitter.on('rainbow_namechanged', (info) => {
+    console.log('#############')
+    console.log('Bubble rainbow_namechanged received', info)
+    console.log('#############')
+})
 
